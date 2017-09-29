49bea5e817eb4a02b20fd441765d206daf4342c7
Kasimir Ilmonen <jyrysanteri@hotmail.com>
2017-09-29 22:42:29 +0300

    Hahmon liikkuminen, kaalikuva, elamat

--- peli.js
+++ peli.js
@@ -2,20 +2,20 @@
 
 var game = new Phaser.Game(window.innerWidth * window.devicePixelRatio, window.innerHeight * window.devicePixelRatio, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 
-
 var kentta;
 
 function preload() {
     // Ladataan resurssit
     game.load.image('etana','kuvat/Ilkea_etana.png');
+    game.load.image('kaali','kuvat/cabbage.png');
 }
 
 class Hahmo {
     
     //Alustaa hahmon
-    constructor(kuva, idnum, x, y) {
+    constructor(kuva, nimi, x, y) {
         this.kuva = kuva;
-        this.idnum = idnum;
+        this.nimi = nimi;
         this.x = x;
         this.y = y;
         this.s = game.add.sprite(x, y, this.kuva);
@@ -23,10 +23,6 @@ class Hahmo {
     
     //Piirtää hahmon
     piirra(x, y, xl, yl) {
-        
-        //var s = game.add.sprite(x + this.x * xl, y + this.y * yl, this.kuva);
-        
-        
         var h = this.s.height;
         var w = this.s.width;
         
@@ -37,8 +33,12 @@ class Hahmo {
             var skaalain = (yl - (yl / 10.0)) / h;
         }
         
-        if (skaalain != 1) {
-            this.s.scale.setTo(skaalain,skaalain);// = yl - (yl / 10.0);
+        if (this.nimi == "kaali") {
+            skaalain = skaalain * 0.6;//Kaalit ovat aina pienempiä
+        }
+        
+        if (Math.abs(skaalain - 1) > 0.05) {
+            this.s.scale.setTo(skaalain,skaalain);
         }
         
         this.s.x = x + this.x * xl + keskita(this.s.width, xl);
@@ -75,6 +75,37 @@ class Kentta {
         this.hahmot.push(hahmo);
     }
     
+    
+    //Liikuttaa annettua hahmoa annettuun suuntaan.
+    liikuta(nimi, suunta){
+        for (var i = 0; i < this.hahmot.length; i++){
+            var h = this.hahmot[i];
+            if (h.nimi == nimi) {
+                if (suunta == suunnat.OIKEA) {
+                    if (++h.x >= this.leveys) {
+                        h.x = 0;
+                    }
+                }
+                if (suunta == suunnat.VASEN) {
+                    if (--h.x < 0) {
+                        h.x = this.leveys - 1;
+                    }
+                }
+                if (suunta == suunnat.YLOS) {
+                    if (--h.y < 0) {
+                        h.y = 0;
+                    }
+                }
+                if (suunta == suunnat.ALAS) {
+                    if (++h.y >= this.korkeus) {
+                        h.y = this.korkeus - 1;
+                    }
+                }
+            }
+        }    
+    }
+    
+    //Päivittää piirtoalueen.
     paivitaAlue(alue_x1, alue_x2, alue_y1, alue_y2){
         this.alue_x1 = alue_x1;
         this.alue_x2 = alue_x2;
@@ -104,8 +135,36 @@ class Kentta {
         for (var i = 0; i < this.hahmot.length; i++) {
             this.hahmot[i].piirra(x, y, ruudun_leveys, ruudun_leveys);
         }
+    }
+    
+    //Nollaa kentän
+    nollaaKentta(){
+        this.g.clear();
+        for (var i = 0; i < this.hahmot.length; i++){
+            this.hahmot[i].s.destroy();
+        }
+        this.hahmot = [];
+    }
+    
+    //Tarkistaa onko nimillä a ja b olevat hahmot samassa ruudussa.
+    onkoSamassa(a, b){
+        var a_x;
+        var a_y;
+        var b_x;
+        var b_y;
         
+        for (var i = 0; i < this.hahmot.length; i++){
+            if (this.hahmot[i].nimi == a) {
+                a_x = this.hahmot[i].x;
+                a_y = this.hahmot[i].y;
+            }
+            if (this.hahmot[i].nimi == b) {
+                b_x = this.hahmot[i].x;
+                b_y = this.hahmot[i].y;
+            }
+        }
         
+        return (a_x === b_x && a_y === b_y);
     }
     
 }
@@ -116,8 +175,8 @@ function luoKentta() {
     var yruutuja = 3;
     
     var kentta = new Kentta(10,window.innerWidth - 10, 0, window.innerHeight, xruutuja,yruutuja);
-    var hahmo = new Hahmo('etana', 1, 1, 1);
-    var hahmo2 = new Hahmo('etana', 1, 2, 2);
+    var hahmo = new Hahmo('kaali', "kaali", 0, 0);
+    var hahmo2 = new Hahmo('etana', "etana", 2, 2);
     
     kentta.lisaaHahmo(hahmo);
     kentta.lisaaHahmo(hahmo2); 
@@ -126,33 +185,69 @@ function luoKentta() {
 }
 
 
-
 function create() {
-    // Asetetaan skaalaukseen liittyvät suhdeluvut
-    
     game.stage.backgroundColor = "#4488AA";// Asetetaan taustaväri
     
     kentta = luoKentta();
-    window.addEventListener('resize',resize(), true);
-    
-    kentta.piirra();
-
- 
 }
 
-var n = 1;
 
-var otukset = [];
+var elamat = 3;
 
 // Muistina, että mihin suuntaan ollaan menossa.
 var olikoOikea = true;
 
-function update() {
-    //kentta.width = 
-    kentta.piirra();
-    resize();
-}
+var edellinenJohonkinYlhaalla = [true,true,true,true]
 
+var suunnat = {
+    YLOS: 0,
+    ALAS: 1,
+    VASEN: 2,
+    OIKEA: 3   
+};
+
+function update() {
+    //Luetaan kontrollit ja liikutaan sen mukaan.
+    if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT) && edellinenJohonkinYlhaalla[3]) {
+        kentta.liikuta("kaali",suunnat.OIKEA);
+    }
+    else {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT) && edellinenJohonkinYlhaalla[2]) {
+            kentta.liikuta("kaali",suunnat.VASEN);
+        }
+        else {
+            if (game.input.keyboard.isDown(Phaser.Keyboard.UP) && edellinenJohonkinYlhaalla[0]) {
+                kentta.liikuta("kaali",suunnat.YLOS);
+            }
+            else {
+                if (game.input.keyboard.isDown(Phaser.Keyboard.DOWN) && edellinenJohonkinYlhaalla[1]) {
+                    kentta.liikuta("kaali",suunnat.ALAS);
+                }
+            }
+        }
+    }
+    
+    //Tallentaa onko pohjassa
+    var apu = [Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.LEFT, Phaser.Keyboard.RIGHT]
+    for(var i = 0; i < edellinenJohonkinYlhaalla.length; i++){
+        edellinenJohonkinYlhaalla[i] = !game.input.keyboard.isDown(apu[i]);
+    }
+    
+    //Tarkistaa tuliko syödyksi
+    if (kentta.onkoSamassa("kaali", "etana")) {
+        kentta.nollaaKentta();
+        
+        if (--elamat <= 0){
+            peliOhi();
+        }
+        else{
+            kentta = luoKentta();
+        }
+    }
+    
+    kentta.piirra();
+    //resize(); //Ei toimi. Aiheuttaa värinää.
+}
 
 
 function resize(){
@@ -168,7 +263,5 @@ function resize(){
 
 // Pelin loppuminen
 function peliOhi(){
-    game.world.removeAll(); //Poistaa kaiken
-    
-    game.add.text(0,0, "Hävisit. Olit surkea.");//Kirjoittaa lopputekstin
+    game.add.text(0,0, "Hävisit. Sinusta tehtiin kaalikääryleitä.");//Kirjoittaa lopputekstin
 }
